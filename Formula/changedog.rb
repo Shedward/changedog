0d97a7c0bfb837e368e58c70581da370329bce2d
Vlad Maltsev <maltsevvn@m2.ru>
2020-12-17 15:19:11 +0300

    Release v1.2

--- Formula/changedog.rb
+++ Formula/changedog.rb
@@ -1,7 +1,7 @@
 class Changedog < Formula
   desc "Writes list of recent releases with jira tasks"
-  url "git@gitlab.m2.ru:vtblife/mobile/common/changedog.git", :using => :git, :tag => "v1.1"
-  version "1.1"
+  url "git@gitlab.m2.ru:vtblife/mobile/common/changedog.git", :using => :git, :tag => "v1.2"
+  version "1.2"
 
   uses_from_macos "swift" => :build
 
